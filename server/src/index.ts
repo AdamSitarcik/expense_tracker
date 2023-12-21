281183f2697a3491d19d0dc6839610aeb7bc022b
Adam Sitarcik <adamsitarcik21@gmail.com>
2023-12-21 21:24:33 +0100

    setup routes for operations which will be implemented

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,24 +6,31 @@ const PORT = 8080;
 const app = express();
 const prisma = new PrismaClient();
 
-app.use(express.json())
+app.use(express.json());
 
+// Test
 app.get('/api', (req, res) => {
     res.json({ message: 'Hello world' });
 });
 
-app.post('/api/expense', async (req, res) => {
-    const { name, email } = req.body;
+app.post('/api/user');
+app.patch('/api/user');
+app.delete('/api/user');
 
-    await prisma.user.create({
-        data: {
-            name: name,
-            email: email,
-        },
-    });
+// Expenses API
+app.get('/api/expense');
+app.post('/api/expense', async (req, res) => {
+    const { price, categoryId } = req.body;
 
     res.json({ message: 'OK' });
 });
+app.patch('/api/expense');
+app.delete('/api/expense');
+
+// Categories API
+app.get('/api/category');
+app.post('/api/category');
+app.delete('/api/category');
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
