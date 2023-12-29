23985b6c065ba77127ec6da744fd5fb21e2a4b86
Adam Sitarcik <adamsitarcik21@gmail.com>
2023-12-29 20:28:25 +0100

    setup landing page with a test for fetching data from the DB

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,46 @@
-import type { NextPage } from 'next';
+'use client';
 
-const Page : NextPage = ()=> {
-    return <div>Landing</div>;
-}
+import type { NextPage } from 'next';
+import { useState } from 'react';
+
+type ExpenseType = {
+    price: string;
+};
+
+const Page: NextPage = () => {
+    const [prices, setPrices] = useState<string[]>([]);
+
+    const handleSubmit = async () => {
+        const data = await fetch('/api/expense', { method: 'GET' });
+
+        const { expenses } = await data.json();
+
+        expenses.map((expense: ExpenseType) => {
+            setPrices((prevPrices) => [...prevPrices, expense.price]);
+        });
+    };
+
+    return (
+        <div>
+            {/* <form
+                onSubmit={() => {
+                    handleSubmit();
+                }}
+            >
+
+            </form> */}
+            <div>
+                Prices:
+                <ul>
+                    {prices.map((expense, index) => {
+
+                        return <li key={index}>{expense}</li>;
+                    })}
+                </ul>
+            </div>
+            <button onClick={() => handleSubmit()}>Get expenses</button>
+        </div>
+    );
+};
 
 export default Page;
