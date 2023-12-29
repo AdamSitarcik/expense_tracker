57ea8f1a0de8655d5eaea671c1c7113c2fba9c78
Adam Sitarcik <adamsitarcik21@gmail.com>
2023-12-29 20:29:05 +0100

    start seting up NextAuth, doesnt work so far

--- client/src/app/api/auth/[...nextauth].ts
+++ client/src/app/api/auth/[...nextauth].ts
@@ -1,6 +1,5 @@
-import NextAuth from 'next-auth/next';
 import GoogleProvider from 'next-auth/providers/google';
-import type { NextAuthOptions } from 'next-auth';
+import { NextAuthOptions, getServerSession } from 'next-auth';
 
 export const authOptions: NextAuthOptions = {
     providers: [
@@ -11,13 +10,50 @@ export const authOptions: NextAuthOptions = {
     ],
     pages: {
         signIn: '/sign-in',
-        signOut: '/sign-out',
-        newUser: '/new-user',
     },
     callbacks: {
+        session({ token, session }) {
+            if (token) {
+                session.user.id = token.id;
+                session.user.email = token.email;
+                // session.user?.name = token.name;
+                // session.user?.username = token.username;
+            }
+
+            return session;
+        },
+        async jwt({ token, user }) {
+            const dbUser = await fetch('/api/user', {
+                method: 'GET',
+                body: user.id,
+            });
+
+            if (!dbUser) {
+                if (user) {
+                    token.id = user.id;
+                }
+                return token;
+            }
+
+            return {
+                id: dbUser.id,
+                email: dbUser.email,
+                name: dbUser?.name,
+                username: dbUser?.username,
+            };
+        },
         signIn({ user }) {
             console.log(user.id);
+            //put a post request to the API with user data -> there find it in DB; if it exist just return, else save it to DB
+            fetch('/api/sign-in', {
+                method: 'POST',
+                body: JSON.stringify({ user }),
+            });
+
             return true;
+        },
+        redirect() {
+            return '/';
         },
     },
     session: {
@@ -25,4 +61,7 @@ export const authOptions: NextAuthOptions = {
     },
 };
 
-export default NextAuth(authOptions);
+// export async function getCurrentSession() {
+//     const session = await getServerSession(authOptions);
+//     return session;
+// }
