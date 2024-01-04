d29c6cd8337d3718eca1c0226c34b09fb04b3f4d
Adam Sitarcik <adamsitarcik21@gmail.com>
2024-01-04 15:48:37 +0100

    remove rewrites function from next config - use .env variable for server address

--- client/next.config.js
+++ client/next.config.js
@@ -1,18 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    async rewrites() {
-        return [
-            {
-                source: '/api/expense',
-                destination: 'http://localhost:8080/api/expense',
-            },
-            {
-                source: '/api/user',
-                destination: 'http://localhost:8080/api/user',
-            },
-        ];
-    },
 };
 
 module.exports = nextConfig;
