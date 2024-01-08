f02662089bbcc499240131a5008226f3c2510a47
Adam Sitarcik <adamsitarcik21@gmail.com>
2024-01-08 17:52:25 +0100

    change redirection url of "get started" button on landing page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -24,7 +24,7 @@ const Page: NextPage = async () => {
                 </p>
                 <div className='flex gap-3'>
                     <Link
-                        href='/sign-in'
+                        href='/register'
                         className={cn(buttonVariants({ size: 'lg' }), 'w-36')}
                     >
                         Get started
