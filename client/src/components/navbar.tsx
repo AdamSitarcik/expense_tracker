ce0ee60fadb3c6d718885bfa03d6b6d92702b630
Adam Sitarcik <adamsitarcik21@gmail.com>
2024-01-12 14:22:10 +0100

    remove one obsolete div from navbar - hydration error is still present; add either logout or login btn, based if there is active session

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -1,28 +1,24 @@
 import { getCurrentUser } from '@/lib/session';
 import Link from 'next/link';
 import { Logo } from './icons';
-import { buttonVariants } from './button';
-import { cn } from '@/lib/utils';
-import { signIn } from 'next-auth/react';
 import { LoginBtn } from './loginBtn';
+import { LogoutBtn } from './logoutBtn';
 
 export const Navbar = async () => {
     const user = await getCurrentUser();
 
     return (
-        <div className='top-0 inset-x-0 h-18 z-10 py-2 bg-background text-foreground px-8 '>
-            <div className='container w-full h-full flex items-center justify-center  sm:justify-between gap-2'>
-                <Link
-                    href='/'
-                    className='flex gap-2 items-center flex-col sm:flex-row'
-                >
-                    <Logo className='h-16 w-16' />
-                    <p className=' text-zinc-700 text-md font-medium  text-2xl'>
-                        Expenser
-                    </p>
-                </Link>
-                <LoginBtn />
-            </div>
+        <div className='top-0 inset-x-0 h-18 z-10 py-2 bg-background text-foreground px-8 container w-full h-full flex items-center justify-center  sm:justify-between gap-2'>
+            <Link
+                href='/'
+                className='flex gap-2 items-center flex-col sm:flex-row'
+            >
+                <Logo className='h-16 w-16' />
+                <p className=' text-zinc-700 text-md font-medium  text-2xl'>
+                    Expenser
+                </p>
+            </Link>
+            {user?.email ? <LogoutBtn /> : <LoginBtn />}
         </div>
     );
 };
